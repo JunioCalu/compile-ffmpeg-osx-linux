1954f2ea65c3022174776bd0f46b1b46eae9af9d
Junio Calu <junio.calu@gmail.com>
2025-01-22 18:46:46 -0300

    Add script for install depends from dnf on Almalinux 9.2

---
+++ dnf_install_depends.sh
@@ -0,0 +1,10 @@
+sudo dnf update -y && sudo dnf install -y \
+  @development-tools autoconf automake libtool pkgconfig texinfo yasm cmake curl git wget gperf \
+  ninja-build cargo cargo-c nasm meson rsync vim-common clang ladspa-devel gmp-devel libdrm-devel \
+  libiec61883-devel jack-devel libmodplug-devel gsm-devel libbs2b-devel pulseaudio-libs-devel snappy-devel \
+  libssh-devel libtheora-devel libv4l-devel vid.stab-devel libvorbis-devel onevpl-devel libwebp-devel \
+  xvidcore-devel ocl-icd-devel mesa-libGL-devel vulkan-devel libjxl-devel shaderc-devel libxcb-devel \
+  fontconfig-devel freetype-devel fribidi-devel libbluray-devel aom-devel lame-devel openjpeg2-devel \
+  speex-devel soxr-devel librsvg2-devel rubberband-devel dav1d-devel svt-av1-devel libxml2-devel \
+  libzstd-devel zlib-devel twolame-devel librist-devel vulkan-tools openssl-devel python3-Cython rav1e-devel \
+  libplacebo-devel zimg-devel libvpx-devel libass-devel x264-devel x265-devel opus-devel
---
+++ dnf_install_depends_para_teste.sh
@@ -0,0 +1,8 @@
+sudo dnf install -y \
+  @development-tools \
+  autoconf automake libtool pkgconfig texinfo yasm cmake curl git wget gperf \
+  ninja-build cargo cargo-c nasm meson rsync vim-common clang ladspa-devel gmp-devel \
+  libdrm-devel libiec61883-devel jack-audio-connection-kit-devel libmodplug-devel gsm-devel \
+  libbs2b-devel pulseaudio-libs-devel snappy-devel libssh-devel libtheora-devel \
+  libv4l-devel vid.stab-devel libvorbis-devel onevpl-devel libwebp-devel xvidcore-devel \
+  ocl-icd-devel mesa-libGL-devel vulkan-devel libjxl-devel shaderc-devel libxcb-devel
