eede00f57f30e6b31ee5055aba47a51ef018e1f2
Junio Calu <junio.calu@gmail.com>
2025-01-23 20:39:05 -0300

    Add command_line_ffmpeg_cuda.sh and full_depends_ffmpeg_without_cuda.sh

---
+++ full_depends_ffmpeg_without_cuda.sh
@@ -0,0 +1,77 @@
+sudo apt install autoconf automake autopoint autotools-dev build-essential bzip2 clang clang-14 cleancss \
+  comerr-dev cpp cpp-11 debhelper debugedit dh-autoreconf dh-strip-nondeterminism diffstat doxygen \
+  dpkg-dev dwz flite1-dev frei0r-plugins-dev g++ g++-11 gcc gcc-11 gcc-11-base gettext \
+  gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-harfbuzz-0.0 gir1.2-ibus-1.0 gir1.2-rsvg-2.0 \
+  icu-devtools intltool-debian krb5-multidev ladspa-sdk lib32gcc-s1 lib32stdc++6 libaliased-perl \
+  libaom-dev libapt-pkg-perl libarchive-dev libarchive-zip-perl libaribb24-0 libaribb24-dev \
+  libasan6 libasound2-dev libass-dev libatomic1 libavahi-client3 libavahi-common-data \
+  libavahi-common3 libavc1394-dev libb-hooks-endofscope-perl libb-hooks-op-check-perl libblkid-dev \
+  libbluray-dev libbrotli-dev libbs2b-dev libbsd-dev libbz2-dev libc-dev-bin libc6-dev libc6-i386 \
+  libcaca-dev libcairo-script-interpreter2 libcairo2-dev libcapture-tiny-perl libcc1-0 \
+  libcdio-cdda-dev libcdio-dev libcdio-paranoia-dev libchromaprint-dev libclang-common-14-dev \
+  libclang-cpp14 libclang1-14 libclass-data-inheritable-perl libclass-method-modifiers-perl \
+  libclass-xsaccessor-perl libclone-perl libcodec2-dev libconfig-tiny-perl libconst-fast-perl \
+  libcpanel-json-xs-perl libcrypt-dev libcups2 libdata-dpath-perl libdata-messagepack-perl \
+  libdata-optlist-perl libdata-validate-domain-perl libdata-validate-ip-perl \
+  libdata-validate-uri-perl libdav1d-dev libdbus-1-dev libdc1394-dev libdebhelper-perl \
+  libdecor-0-dev libdeflate-dev libdevel-callchecker-perl libdevel-size-perl \
+  libdevel-stacktrace-perl libdpkg-perl libdrm-dev libdynaloader-functions-perl libegl-dev \
+  libegl-mesa0 libegl1 libegl1-mesa-dev libemail-address-xs-perl libevent-2.1-7 \
+  libexception-class-perl libexpat1-dev libexporter-tiny-perl libffi-dev libffmpeg-nvenc-dev \
+  libfile-basedir-perl libfile-find-rule-perl libfile-stripnondeterminism-perl libfont-ttf-perl \
+  libfontconfig-dev libfontconfig1-dev libfreetype-dev libfreetype6-dev libfribidi-dev libgbm-dev \
+  libgc1 libgcc-11-dev libgcrypt20-dev libgdk-pixbuf-2.0-dev libgif7 libgl-dev libgl1-mesa-dev \
+  libgles-dev libgles1 libgles2 libglib2.0-dev libglib2.0-dev-bin libglu1-mesa libglu1-mesa-dev \
+  libglvnd-core-dev libglvnd-dev libglx-dev libgme-dev libgmp-dev libgmpxx4ldbl libgnutls-dane0 \
+  libgnutls-openssl27 libgnutls28-dev libgpg-error-dev libgraphite2-dev libgsm1-dev \
+  libgssrpc4 libharfbuzz-dev libharfbuzz-gobject0 libharfbuzz-icu0 libhtml-html5-entities-perl \
+  libibus-1.0-5 libibus-1.0-dev libice-dev libice6 libicu-dev libidn2-dev libiec61883-dev \
+  libimport-into-perl libio-interactive-perl libio-prompt-tiny-perl libio-string-perl \
+  libipc-run3-perl libipc-system-simple-perl libisl23 libiterator-perl libiterator-util-perl \
+  libitm1 libjack-jackd2-dev libjbig-dev libjpeg-dev libjpeg-turbo8-dev libjpeg8-dev libjs-inherits \
+  libjs-source-map libjson-maybexs-perl libkadm5clnt-mit12 libkadm5srv-mit12 libkdb5-10 libkrb5-dev \
+  libldb2 liblept5 libleptonica-dev liblilv-dev liblist-compare-perl liblist-moreutils-perl \
+  liblist-moreutils-xs-perl liblist-someutils-perl liblist-utilsby-perl libllvm14 liblsan0 \
+  liblzma-dev libmarkdown2 libmd-dev libmfx-dev libmodule-implementation-perl \
+  libmodule-runtime-perl libmoo-perl libmoox-aliases-perl libmount-dev libmouse-perl libmp3lame-dev \
+  libmpc3 libmpg123-dev libmysofa-dev libnamespace-clean-perl nodejs libnode109 libnet-domain-tld-perl \
+  libnet-ipv6addr-perl libnet-netmask-perl libnetaddr-ip-perl libnorm-dev libnsl-dev \
+  libnumber-compare-perl libobjc-11-dev libobjc4 libogg-dev libomxil-bellagio-dev \
+  libomxil-bellagio0 libopenal-dev libopencore-amrnb-dev libopencore-amrnb0 libopencore-amrwb-dev \
+  libopencore-amrwb0 libopengl-dev libopengl0 libopenjp2-7-dev libopenmpt-dev libopus-dev \
+  libout123-0 libp11-kit-dev libpackage-stash-perl libparams-classify-perl libparams-util-perl \
+  libpath-tiny-perl libpciaccess-dev libpcre16-3 libpcre2-16-0 libpcre2-32-0 libpcre2-dev \
+  libpcre2-posix3 libpcre3-dev libpcre32-3 libpcrecpp0v5 libperlio-gzip-perl \
+  libperlio-utf8-strict-perl libpgm-dev libpixman-1-dev libpng-dev libpocketsphinx-dev \
+  libpthread-stubs0-dev libpulse-dev libpulse-mainloop-glib0 librabbitmq-dev libraw1394-dev \
+  librole-tiny-perl librsvg2-dev librubberband-dev libsamplerate0-dev libsctp-dev libsctp1 \
+  libsdl2-dev libselinux1-dev libsepol-dev libserd-dev libsereal-decoder-perl \
+  libsereal-encoder-perl libset-scalar-perl libshine-dev libslang2-dev libsm-dev libsm6 \
+  libsmbclient libsmbclient-dev libsnappy-dev libsndio-dev libsodium-dev libsord-dev \
+  libsort-versions-perl libsoxr-dev libsoxr-lsr0 libspeex-dev libsphinxbase-dev libsratom-dev \
+  libsrt-gnutls-dev libssh-gcrypt-dev libssl-dev libstdc++-11-dev libstrictures-perl \
+  libsub-exporter-perl libsub-exporter-progressive-perl libsub-identify-perl libsub-install-perl \
+  libsub-name-perl libsub-override-perl libsub-quote-perl libsyn123-0 libsyntax-keyword-try-perl \
+  libtalloc2 libtasn1-6-dev libtdb1 libtesseract-dev libtevent0 libtext-glob-perl \
+  libtext-levenshteinxs-perl libtext-markdown-discount-perl libtext-unidecode-perl \
+  libtext-xslate-perl libtheora-dev libtiff-dev libtime-duration-perl \
+  libtime-moment-perl libtimedate-perl libtirpc-dev libtool libtry-tiny-perl libtsan0 \
+  libtwolame-dev libubsan1 libudev-dev libudfread-dev libunbound8 libunicode-utf8-perl liburi-perl \
+  libva-dev libva-glx2 libva-wayland2 libvariable-magic-perl libvdpau-dev libvidstab-dev \
+  libvo-amrwbenc-dev libvo-amrwbenc0 libvorbis-dev libvpx-dev libwayland-bin libwayland-dev \
+  libwbclient0 libwebp-dev libwebpdemux2 libx11-dev libx264-dev libx265-dev libxapian30 libxau-dev \
+  libxcb-render0-dev libxcb-shape0-dev libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev \
+  libxcursor-dev libxdmcp-dev libxext-dev libxfixes-dev libxi-dev libxinerama-dev libxkbcommon-dev \
+  libxml-libxml-perl libxml-namespacesupport-perl libxml-sax-base-perl libxml-sax-perl libxml2-dev \
+  libxrandr-dev libxrender-dev libxs-parse-keyword-perl libxss-dev libxt-dev libxt6 libxv-dev \
+  libxvidcore-dev libxvmc-dev libxvmc1 libxxf86vm-dev libyaml-libyaml-perl libzimg-dev libzmq3-dev \
+  libzvbi-dev lintian linux-libc-dev llvm-14-linker-tools lto-disabled-list lv2-dev lzip lzop m4 \
+  make nasm nettle-dev node-anymatch node-async-each node-balanced-match node-binary-extensions \
+  node-brace-expansion node-braces node-chokidar node-clean-css node-commander node-fill-range \
+  node-fs.realpath node-glob node-glob-parent node-graceful-fs node-inflight node-inherits \
+  node-is-binary-path node-is-buffer node-is-extglob node-is-glob node-is-number node-kind-of \
+  node-less node-minimatch node-normalize-path node-once node-path-dirname node-path-is-absolute \
+  node-readdirp node-repeat-string node-set-immediate-shim node-source-map node-to-regex-range \
+  node-tslib node-wrappy nodejs ocl-icd-opencl-dev opencl-c-headers opencl-clhpp-headers patchutils \
+  pkg-config pkg-kde-tools po-debconf python3-ldb python3-talloc rpcsvc-proto samba-libs t1utils \
+  tex-common texinfo tree unzip uuid-dev x11proto-dev xorg-sgml-doctools xtrans-dev zlib1g-dev libvpl-dev libvpl2
